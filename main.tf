2b8f103b6466577a60fb7a5de853a0bb50033941
Alexandre Weiss <alexandreweiss@users.noreply.github.com>
2024-12-26 12:11:19 +0100

    Update main.tf
    Added left and right for GW transit

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ data "azurerm_virtual_network" "right_vnet_data" {
 resource "azurerm_virtual_network_peering" "left_right" {
   allow_forwarded_traffic = var.allow_forwarded_traffic
   allow_virtual_network_access = var.allow_virtual_network_access
-  allow_gateway_transit = var.allow_gateway_transit
-  use_remote_gateways = var.use_remote_gateways
+  left_allow_gateway_transit = var.allow_gateway_transit
+  left_use_remote_gateways = var.use_remote_gateways
   resource_group_name = var.left_vnet_resource_group_name
   virtual_network_name = var.left_vnet_name
   remote_virtual_network_id = data.azurerm_virtual_network.right_vnet_data.id
@@ -22,8 +22,8 @@ resource "azurerm_virtual_network_peering" "left_right" {
 resource "azurerm_virtual_network_peering" "right_left" {
   allow_forwarded_traffic = var.allow_forwarded_traffic
   allow_virtual_network_access = var.allow_virtual_network_access
-  allow_gateway_transit = var.allow_gateway_transit
-  use_remote_gateways = var.use_remote_gateways
+  right_allow_gateway_transit = var.allow_gateway_transit
+  right_use_remote_gateways = var.use_remote_gateways
   resource_group_name = var.right_vnet_resource_group_name
   virtual_network_name = var.right_vnet_name
   remote_virtual_network_id = data.azurerm_virtual_network.left_vnet_data.id
