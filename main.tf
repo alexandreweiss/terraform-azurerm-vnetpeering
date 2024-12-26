917d500cfa3d719af691985219783a0791bafb97
Alexandre Weiss <alexandreweiss@users.noreply.github.com>
2024-12-26 12:21:22 +0100

    Update main.tf
    update

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_virtual_network_peering" "left_right" {
 
 resource "azurerm_virtual_network_peering" "right_left" {
   allow_forwarded_traffic = var.allow_forwarded_traffic
-  allow_virtual_network_access = var.right_allow_virtual_network_access
+  allow_virtual_network_access = var.allow_virtual_network_access
   allow_gateway_transit = var.right_allow_gateway_transit
   use_remote_gateways = var.right_use_remote_gateways
   resource_group_name = var.right_vnet_resource_group_name
